9ec9f85cb3bd2b3f72be78be357d225cd5a9b4b4
ant6 <ant6ant@gmail.com>
2016-04-26 22:20:49 +0200

    Now PieChart extends JPanel

--- src/main/java/pl/edu/agh/piechart/PieChart.java
+++ src/main/java/pl/edu/agh/piechart/PieChart.java
@@ -1,107 +1,56 @@
 package pl.edu.agh.piechart;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
-import org.jfree.util.Rotation;
 
+import javax.swing.JPanel;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Pie chart generator.
- */
-@SuppressWarnings("WeakerAccess")
-public class PieChart extends ApplicationFrame {
+public class PieChart extends JPanel {
 
-    private String title;
-    private Map<String, Double> data;
-    private PieDataset dataset;
-
-    private int WIN_WIDTH = 640;
-    private int WIN_HEIGHT = 480;
+    private Map<String, Double> data = new HashMap<String, Double>();
+    private PiePlot3D piePlot3D = new PiePlot3D();
 
     /**
-     * Creates a pie chart.
-     *
-     * @param title  the frame title.
+     * Creates a pie chart with default size.
+     * @param title the frame title.
      */
     public PieChart(final String title) {
-        super(title);
-        this.title = title;
-        this.data = new HashMap<String, Double>();
-        createDataSet(); // empty one
-        updatePieChart();
-        RefineryUtilities.centerFrameOnScreen(this);
-        setVisible(true);
+        pieChartInit(title, 500, 500);
+    }
+
+    public PieChart(final  String title, final int width, final int height){
+        pieChartInit(title, width, height);
     }
 
     /**
-     * Set value for a key name, if it does not exist - add it.
+     * Set value for a key name, if it does not exist - add it and update data set.
      * @param name  item name
      * @param value value, negative won't be shown
      */
     public void setChartValue(final String name, final double value) {
-        // add value or alter in data map
         this.data.put(name, value);
-        // recreate PieDataSet
-        createDataSet();
-        // Update PieChart
-        updatePieChart();
+        piePlot3D.setDataset(makeDataSet());
+        repaint();
     }
 
-    /**
-     * Change window height and width.
-     */
-    public void setChartSize(final int width, final int height) {
-        this.WIN_WIDTH = width;
-        this.WIN_HEIGHT = height;
-        updatePieChart();
+    private void pieChartInit(final String title, final int width, final int height) {
+        JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, piePlot3D, true);
+        this.add(new ChartPanel(chart,
+                                width, height, width, height, width, height,
+                                false, true, true, true, true, true));
+        repaint();
     }
 
-    private void createDataSet(){
+    private PieDataset makeDataSet() {
         DefaultPieDataset result = new DefaultPieDataset();
         for (String name : this.data.keySet()) {
             result.setValue(name, this.data.get(name));
         }
-        this.dataset = result;
-    }
-
-    /**
-     * Recreates PieChart with current data set.
-     */
-    private void updatePieChart(){
-        JFreeChart chart = createChart(this.title, this.dataset);
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(WIN_WIDTH, WIN_HEIGHT));
-        setContentPane(chartPanel);
-        pack();
-    }
-
-    /**
-     * Creates the PieChart.
-     */
-    private JFreeChart createChart(final String title, final PieDataset dataset) {
-        final JFreeChart chart = ChartFactory.createPieChart3D(
-                title,  // chart title
-                dataset,                // data
-                true,                   // include legend
-                true,
-                false
-        );
-
-        final PiePlot3D plot = (PiePlot3D) chart.getPlot();
-        plot.setStartAngle(290);
-        plot.setDirection(Rotation.CLOCKWISE);
-        plot.setForegroundAlpha(0.5f);
-        plot.setNoDataMessage("No data to display");
-        plot.setLabelGenerator(new StandardPieSectionLabelGenerator());
-        return chart;
+        return result;
     }
 }
--- src/main/java/pl/edu/agh/piechart/SamplePieChart.java
+++ src/main/java/pl/edu/agh/piechart/SamplePieChart.java
@@ -1,28 +1,41 @@
 package pl.edu.agh.piechart;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
 
 public class SamplePieChart {
     public static void main(final String[] args) {
-        // 1. init
-        final PieChart demo = new PieChart("Testing...");
-        // 2. (kinda optional, we start with empty data set) set some values
-        demo.setChartValue("JS", 7.2);
-        demo.setChartValue("Ada", -10.2); // won't be shown (negative val)
-        demo.setChartValue("Python", 37.2);
-        demo.setChartValue("C++", 25.0);
-        // wait 5 seconds and alter C++
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-        demo.setChartValue("C++", 50.0);
+        // Create main JFrame for testing
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Create JPanels (PieCharts)
+        PieChart pie = new PieChart("testing", 600, 600);
+        PieChart pie2 = new PieChart("testing 2");
+
+        // add JPanels to JFrame
+        frame.getContentPane().add(pie, BorderLayout.WEST);
+        frame.getContentPane().add(pie2, BorderLayout.EAST);
+        frame.setSize(1100, 700);
+        frame.setVisible(true);
+        frame.setResizable(false);
+
+        // add some data
+        pie.setChartValue("Python", 37);
+        pie2.setChartValue("Rain", 23);
+        pie2.setChartValue("Sunny", 44);
+
+        // wait 3 seconds and add some data
         try {
             Thread.sleep(3000);
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
-        // 3. (optional) change chart size
-        demo.setChartSize(1200, 600);
+        pie.setChartValue("Ada", 220);
+        pie.setChartValue("C", 22);
+        pie.setChartValue("ASM", -22);
+        pie2.setChartValue("Rain", -1);
+        pie2.setChartValue("Cloudy", 2);
     }
 }
